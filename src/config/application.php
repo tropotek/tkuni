b3b44393e1c3f09e9d8513a86087384fd14c7be9
tropotek <info@tropotek.com>
2016-11-14 10:13:24 +1100

    Minor Code Updates - 252s-dev.vet.unimelb.edu.au

--- src/config/application.php
+++ src/config/application.php
@@ -1,21 +1,28 @@
 <?php
 /*
+ * Application default config values
+ * This file should not need to be edited
+ *
  * @author Michael Mifsud <info@tropotek.com>
  * @link http://www.tropotek.com/
  * @license Copyright 2015 Michael Mifsud
  */
 $config = \Tk\Config::getInstance();
 
-/**
- * Config the session using PHP option names prepended with 'session.'
- * @see http://php.net/session.configuration
- */
 include_once(__DIR__ . '/session.php');
 include_once(__DIR__ . '/routes.php');
 
+
+/**************************************
+ * Default app config values
+ **************************************/
+
 /*
- * Setup any default app config values
+ * If you use sub folders in your URL's you
+ * must define the site root paths manually.
  */
+//$config['site.path'] = dirname(dirname(dirname(__FILE__)));
+//$config['site.url'] = dirname($_SERVER['PHP_SELF']);
 
 /*
  * Template folders for pages
@@ -27,15 +34,6 @@ $config['template.public.path'] = '/html/default';
  * Change the system timezone
  */
 //$config['date.timezone'] = 'Australia/Victoria';
-
-/*
- * If you use sub folders in your URL's you 
- * must define the site root paths manually.
- */
-//$config['site.path'] = dirname(dirname(dirname(__FILE__)));
-//$config['site.url'] = dirname($_SERVER['PHP_SELF']);
-
-
 
 
 
