5ee12fe4e817a16f0ca850c3a03e39a6b1bf9c06
mifsudm <mifsudm@unimelb.edu.au>
2018-07-23 12:40:08 +1000

    ~Auto: Commit

--- vendor/ttek/tk-uni/Uni/Listener/PageTemplateHandler.php
+++ vendor/ttek/tk-uni/Uni/Listener/PageTemplateHandler.php
@@ -1,8 +1,6 @@
 <?php
 namespace Uni\Listener;
 
-use Tk\Event\Subscriber;
-use Tk\Kernel\KernelEvents;
 
 /**
  * This object helps cleanup the structure of the controller code
@@ -11,35 +9,32 @@ use Tk\Kernel\KernelEvents;
  * @link http://www.tropotek.com/
  * @license Copyright 2015 Michael Mifsud
  */
-class PageTemplateHandler implements Subscriber
+class PageTemplateHandler extends \Bs\Listener\PageTemplateHandler
 {
 
     /**
      * @param \Tk\Event\Event $event
-     * @throws \Tk\Db\Exception
-     * @throws \Tk\Exception
+     * @throws \Exception
      */
-    public function onPageShow(\Tk\Event\Event $event)
+    public function showPage(\Tk\Event\Event $event)
     {
+        parent::showPage($event);
         $controller = $event->get('controller');
         if ($controller instanceof \Bs\Controller\Iface) {
             $page = $controller->getPage();
             if (!$page) return;
             $template = $page->getTemplate();
-            /** @var \Bs\Db\User $user */
+            /** @var \Uni\Db\User $user */
             $user = $controller->getUser();
-
 
             // Add anything to the page template here ...
             if ($user) {
                 $template->setAttr('dashUrl', 'href', \Uni\Uri::createHomeUrl('/index.html'));
                 $institutionId = $this->getConfig()->getInstitutionId();
                 $subjectId = $this->getConfig()->getSubjectId();
-                $role = $user->getRole();
                 $js = <<<JS
 config.subjectId = $subjectId;
 config.institutionId = $institutionId;
-config.role = '$role';
 JS;
                 $template->appendJs($js, array('data-jsl-priority' => -1000));
             }
@@ -49,22 +44,11 @@ JS;
 
 
     /**
-     * @return \App\Config|\Tk\Config
+     * @return \Uni\Config|\Tk\Config
      */
     public function getConfig()
     {
-        return \App\Config::getInstance();
+        return \Uni\Config::getInstance();
     }
-    
-    /**
-     * getSubscribedEvents
-     * 
-     * @return array
-     */
-    public static function getSubscribedEvents()
-    {
-        return array(
-            \Tk\PageEvents::PAGE_SHOW =>  array('onPageShow', 0)
-        );
-    }
+
 }
