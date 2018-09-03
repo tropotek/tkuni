f27023b00a1a1591eaf1658c92fd58a190fa9ddd
mifsudm <mifsudm@unimelb.edu.au>
2018-09-03 10:15:57 +1000

    ~Auto: Commit

--- html/app/js/app.js
+++ html/app/js/app.js
@@ -3,12 +3,8 @@
 jQuery(function($) {
 
   //
-  project_core.initDualListBox();
   project_core.initTinymce();
   project_core.initCodemirror();
-  project_core.initMasqueradeConfirm();
-  project_core.initTableDeleteConfirm();
-  project_core.initGrowLikeAlerts();
 
 
 });
