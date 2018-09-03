460e76ff657e23bc614d6a3289dc45c4861e46ca
mifsudm <mifsudm@unimelb.edu.au>
2018-09-04 07:16:40 +1000

    ~Auto: Commit

--- src/App/Controller/Staff/Dashboard.php
+++ src/App/Controller/Staff/Dashboard.php
@@ -71,16 +71,8 @@ class Dashboard extends \Uni\Controller\AdminIface
     {
         $xhtml = <<<HTML
 <div class="">
-  <a href="#" class="togLoader">Click Me</a>
+
   <div class="tk-panel" data-panel-title="Subject List" data-panel-icon="fa fa-university" var="table"></div>
-  <!--<div class="panel panel-default">-->
-    <!--<div class="panel-heading">-->
-      <!--<i class="fa fa-university fa-fw"></i> Subject List-->
-    <!--</div>-->
-    <!--<div class="panel-body">-->
-      <!--<div var="table"></div>-->
-    <!--</div>-->
-  <!--</div>-->
 
 </div>
 HTML;
--- src/App/Controller/Staff/SubjectDashboard.php
+++ src/App/Controller/Staff/SubjectDashboard.php
@@ -45,11 +45,13 @@ class SubjectDashboard extends \Uni\Controller\AdminIface
 
         $this->userTable = \Uni\Table\User::create()->init();
         $this->userTable->findCell('name')->setUrl(\Uni\Uri::createSubjectUrl('/studentEdit.html'));
+        $this->userTable->removeCell('roleId');
+        $this->userTable->appendCell(\Tk\Table\Cell\Date::create('lastLogin'), 'email');
         $filter = array();
         $filter['institutionId'] = $this->getConfig()->getInstitutionId();
         $filter['subjectId'] = $this->getConfig()->getSubjectId();
         $filter['type'] = \Uni\Db\Role::TYPE_STUDENT;
-        $this->userTable->setList($this->userTable->findList($filter));
+        $this->userTable->setList($this->userTable->findList($filter, $this->userTable->getTool('name')));
 
     }
 
@@ -79,12 +81,6 @@ class SubjectDashboard extends \Uni\Controller\AdminIface
 <div>
 
   <div class="tk-panel" data-panel-title="Student List" data-panel-icon="fa fa-users" var="table"></div>
-  <!--<div class="panel panel-default">-->
-    <!--<div class="panel-heading"><i class="fa fa-fw fa-users"></i> <span var="code"></span> Student List</div>-->
-    <!--<div class="panel-body">-->
-      <!--<div var="table"></div>-->
-    <!--</div>-->
-  <!--</div>-->
   
 </div>
 HTML;
