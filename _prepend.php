9e8af61c76539e70ff8f4215991c2f37f63b533b
mifsudm <mifsudm@unimelb.edu.au>
2018-09-04 11:48:58 +1000

    ~Auto: Commit

--- _prepend.php
+++ _prepend.php
@@ -3,16 +3,15 @@
  * @author Michael Mifsud <info@tropotek.com>
  * @link http://www.tropotek.com/
  * @license Copyright 2007 Michael Mifsud
- *
- * Use this as the bootstrap file for all php files
  */
 
-$sitePath = dirname(__FILE__);
+if (!isset($sitePath)) $sitePath = dirname(__FILE__);
+if (!isset($siteUrl)) $siteUrl = null;
+
 /** @var \Composer\Autoload\ClassLoader $composer */
 $composer = include($sitePath . '/vendor/autoload.php');
 
-$config = \App\Config::getInstance();
+$config = \App\Config::getInstance($siteUrl, $sitePath);
 \App\Bootstrap::execute();
 
 $config->set('composer', $composer);
-
