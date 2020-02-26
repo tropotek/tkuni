e652d149473690af0231dff2359010d4b0c255c3
tropotek <info@tropotek.com>
2020-02-26 15:32:59 +1100

    Added LTI 1.3 plugin

--- src/App/Listener/AuthHandler.php
+++ src/App/Listener/AuthHandler.php
@@ -2,6 +2,7 @@
 namespace App\Listener;
 
 
+use Bs\Db\User;
 use Tk\Event\AuthEvent;
 use Tk\Auth\AuthEvents;
 
@@ -184,6 +185,7 @@ class AuthHandler extends \Bs\Listener\AuthHandler
                 $user = $config->createUser();
                 $config->getUserMapper()->mapForm($userData, $user);
                 $user->save();
+                $user->addPermission(\uni\Db\Permission::getPermissionList($user->getType()));
                 $adapter->set('user', $user);
             }
 
