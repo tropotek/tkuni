1cadac21449c292acb4ac27a3167601aa15ac477
tropotek <info@tropotek.com>
2022-05-05 09:16:31 +1000

    ~Auto: Commit

--- _prepend.php
+++ _prepend.php
@@ -7,6 +7,7 @@
 
 if (!isset($sitePath)) $sitePath = dirname(__FILE__);
 if (!isset($siteUrl)) $siteUrl = null;
+if (!is_dir($sitePath . '/vendor')) { die('Please run <code>composer install</code> first to setup and configure this project.'); }
 
 /** @var \Composer\Autoload\ClassLoader $composer */
 $composer = include($sitePath . '/vendor/autoload.php');
